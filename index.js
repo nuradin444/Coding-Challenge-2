b26d2502c2eaccc62c015c6d2aab033fb040d8b1
Nuradin Sheikh <nuradin900@outlook.com>
2020-12-24 13:21:56 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//challenge 1: change color of all buttons
+
+var all_buttons = document.getElementsByTagName("button");
+console.log(all_buttons);
+
+let copyAllButtons = [];
+
+for (let i =0; i < all_buttons.length; i++){
+    copyAllButtons.push(all_buttons[i].classList[i])
+
+}
+
+function buttonColorChange(buttonObject){
+    if(buttonObject.value === 'red'){
+        buttonsRed();
+    }
+    else if(buttonObject.value === 'green'){
+        buttonsGreen();
+    }
+    else if(buttonObject.value === 'reset'){
+        buttonColorReset();
+    }
+    else if(buttonObject === 'random'){
+        randomColors();
+
+    }
+}
+
+function buttonsRed(){
+    for(let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+
+    }
+}
+
+function buttonsGreen(){
+    for(let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+
+    }
+}
+
+function buttonColorReset(){
+    for(let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors(){
+    let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning'];
+}
